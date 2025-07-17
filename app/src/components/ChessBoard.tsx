8fc8f9e4c2b4440619bc1b793fcca5d96a72f641
suwandre <suwandresukijat@gmail.com>
2025-07-17 12:36:58 +0200

    feat: initial navbar skeleton

---
+++ app/src/components/ChessBoard.tsx
@@ -0,0 +1,8 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+export const ChessBoard = () => {
+  return (
+    <p>Chessboard will be added here</p>
+  );
+} 
